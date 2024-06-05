034dfe36799c299167904af51803aa305668e00d
Paulo Malandrim <90413719+paulomalandrim@users.noreply.github.com>
2024-06-05 17:19:40 -0300

    Update index.js

--- node/index.js
+++ node/index.js
@@ -1,24 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'nodedb'
-}
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
-
-const sql = "insert into people(name) values ('Paulo Malandrim')"
-connection.query(sql)
-connection.end
+//callDataBase()
 
 app.get('/', (req,res) => {
-    res.send('<h1>Meu aplicativo de teste em node</h1>')
+
+    callDataBase(res)
 })
 
 app.listen(port, () =>{
     console.log('Rodando na porta ' + port)
 })
+
+function callDataBase(res) {
+    const config = {
+        host: 'db',
+        user: 'root',
+        password: 'root',
+        database: 'nodedb'
+    }    
+    
+    const mysql1 = require('mysql')
+    const connection = mysql1.createConnection(config)
+    const sqlInsert = "insert into people(name) values ('Paulo Malandrim')"
+    connection.query(sqlInsert)
+    connection.end
+
+    const mysql2 = require('mysql')
+    const pool = mysql2.createConnection(config)   
+    const sql = "select * from people"
+    var dados = " "
+    pool.query(sql, function (err, result) {
+        result.forEach(element => {
+            dados += "<h2>" + element.name + "</h2>"
+        })
+        console.log('[QUERY] ' + dados);
+        res.send('<h1>Full Cycle Rocks!</h1>' + dados)
+    })
+    pool.end
+}
